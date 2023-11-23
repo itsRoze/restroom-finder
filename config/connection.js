e55ba1788988e09c92baa345e84b31339ba51c09
itsRoze <elewis9989@gmail.com>
2023-11-22 22:15:13 -0500

    debug

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,7 @@
 const Sequelize = require('sequelize');
 const path = require('path');
 
-require('dotenv').config({path: '../.env'});
-
+require('dotenv').config({ path: '../.env' });
 
 const connection = {
   user: process.env.DB_USER,
@@ -10,27 +9,31 @@ const connection = {
   host: process.env.DB_HOST,
 };
 
-const connectionString= `mysql://${connection.user}:${connection.password}@${connection.host}/restroom_finder_db`
+const connectionString = `mysql://${connection.user}:${connection.password}@${connection.host}/restroom_finder_db`;
+console.log(connectionString);
 
 // create connection to our db ** deleted env user and root
-const sequelize = new Sequelize(connectionString, { dialect: "mysql2", dialectOptions: { ssl: {} } })
-  // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  //     host: 'localhost',
-  //     dialect: 'mysql',
-  //     port: 3306
-  //   });
+const sequelize = new Sequelize(connectionString, {
+  dialect: 'mysql2',
+  dialectOptions: { ssl: {} },
+});
+// : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
 
-    async function authenticate(){
-      try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-    }
+async function authenticate() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
-  authenticate().then(()=> {
-    console.log('yay')
-  })
+authenticate().then(() => {
+  console.log('yay');
+});
 
 module.exports = sequelize;
